6687b30bba3690bb1e59d6d22872ea28fe31857a
xsebastianx <sarnavat1@gmail.com>
2020-12-23 13:46:24 -0500

    Added cards to homepage

--- client/src/components/Home-Body/Body.jsx
+++ client/src/components/Home-Body/Body.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import './Body.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faTruckMoving } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faTools } from '@fortawesome/free-solid-svg-icons';
 
 function HomeBody() {
   return (
     <>
       <div className="body">
         <ul>
-          <li>
-            <div>Buy</div>
+          <li className="buyBody">
+            <div className="buyIcon">
+              <FontAwesomeIcon icon={faHome} size="10x" />
+            </div>
+
+            <div className="buyContent">Buy</div>
           </li>
-          <li>
-            <div>Rent</div>
+          <li className="rentBody">
+            <div className="rentIcon">
+              <FontAwesomeIcon icon={faTruckMoving} size="10x" />
+            </div>
+            <div className="rentContent">Rent</div>
           </li>
-          <li>
-            <div>Community</div>
+          <li className="communityBody">
+            <div className="communityIcon">
+              <FontAwesomeIcon icon={faUsers} size="10x" />
+            </div>
+            <div className="communityContent">Community</div>
           </li>
-          <li>
-            <div>Manage</div>
+          <li className="manageBody">
+            <div className="manageIcon">
+              <FontAwesomeIcon icon={faTools} size="10x" />
+            </div>
+            <div className="manageContent">Manage</div>
           </li>
         </ul>
       </div>
