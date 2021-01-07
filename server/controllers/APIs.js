9903b3f22343dd8e166c0eb87942c3991959662b
xsebastianx <sarnavat1@gmail.com>
2021-01-07 17:21:27 -0500

    Created functional map

--- server/controllers/APIs.js
+++ server/controllers/APIs.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+// const Representatives = require('../db/models/representatives');
+
 exports.getYelpAPI = async (request, response) => {
   try {
     const data = await axios.get(
@@ -22,3 +25,19 @@ exports.searchYelpAPI = async (request, response) => {
     console.log(e);
   }
 };
+
+//Ciceros API
+
+// exports.getRepByZipcode = async (req, res) => {
+//     try {
+//         const address = req.query.address;
+//         const { data } = await axios({
+//             url: `https://cicero.azavea.com/v3.1/official?search_postal=${address}&search_country=US&order=district_type&sort=desc&format=json&key=${process.env.CICERO_API_KEY}`,
+//             method: 'GET'
+//         });
+//         const response = data.response.results.candidates[0];
+//         res.status(200).json(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
--- server/routes/open/APIs.js
+++ server/routes/open/APIs.js
@@ -1,8 +1,15 @@
 const router = require('express').Router(),
-  { getYelpAPI, searchYelpAPI } = require('../../controllers/APIs');
+  { getYelpAPI, searchYelpAPI } = require('../../controllers/APIs'),
+  { getRepByZipcode } = require('../../controllers/APIs');
 
+//Yelp API
 router.get('/api/yelp', getYelpAPI);
 
 router.get('/api/search', searchYelpAPI);
+
+//Ciceros API
+
+// router.get('/representatives/', getRepByZipcode);
+// module.exports = router;
 
 module.exports = router;
